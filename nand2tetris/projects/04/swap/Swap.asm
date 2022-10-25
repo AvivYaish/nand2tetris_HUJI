03eed5468531c139a88c7708264cfbd7d56a2b76
Aviv Yaish <aviv.yaish@mail.huji.ac.il>
2022-10-25 16:46:07 +0300

    Update Swap.asm

--- nand2tetris/projects/04/swap/Swap.asm
+++ nand2tetris/projects/04/swap/Swap.asm
@@ -8,10 +8,9 @@
 // Assumptions:
 // - The array's start address is stored in R14, and R15 contains its length
 // - Each array value x is between -16384 < x < 16384
+// - The min. and max. elements appear only once
 // - The address in R14 is at least >= 2048
 // - R14 + R15 <= 16383
-//
-// Requirements:
 // - Changing R14, R15 is not allowed.
 
 // Put your code here.
